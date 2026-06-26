c4304c19987177a8037c34e5904f243e0b11e718
Serena Kifoula <serena.kifoula@telespazio.com>
2026-06-26 14:33:31 +0200

    Implemented:  a  statement to handle single-character punctuation (brackets, braces, colon, comma).

--- src/lexer.rs
+++ src/lexer.rs
@@ -26,5 +26,26 @@ impl Lexer {
         }
     }
     pub fn tokenize(&mut self) {
+        let len = self.text.chars().count();
+
+        while self.pos < len {
+            let character = self.text.chars().nth(self.pos);
+            match character {
+                Some(c) => {
+                    match c {
+                        c if c.is_whitespace() => continue,
+                        '[' => self.tokens.push(Token::LBracket),
+                        ']' => self.tokens.push(Token::RBracket),
+                        '{' => self.tokens.push(Token::LBrace),
+                        '}' => self.tokens.push(Token::RBrace),
+                        ',' => self.tokens.push(Token::Comma),
+                        ':' => self.tokens.push(Token::Colon),
+                        _ => {},
+                    }
+                },
+                None => break,
+            },
+            self.pos += 1;
+        }
     }
 }

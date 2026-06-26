cd8a45caf056c93772b078a90d3513a11d2e49c0
Serena Kifoula <serena.kifoula@telespazio.com>
2026-06-26 12:36:50 +0200

    Feat: add Token enum and basic Lexer struct

--- src/lexer.rs
+++ src/lexer.rs
@@ -10,3 +10,21 @@ pub enum Token {
     Bool(bool),
     Null,
 }
+
+pub struct Lexer {
+    text : String,
+    pos : usize,
+    tokens : Vec<Token>,
+}
+
+impl Lexer {
+    pub fn new(text: String) -> Self {
+        Lexer {
+            text,
+            pos: 0,
+            tokens: Vec::new(),
+        }
+    }
+    pub fn tokenize(&mut self) {
+    }
+}
